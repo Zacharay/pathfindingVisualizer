23f16f1f968d42b1b78aa2da7c078ece6cd8c320
Zachary <zachary.slupski@wp.pl>
2023-07-20 19:42:44 +0200

    hamiltonian cycle generation completed using kruskal algorithm

--- src/Algorithms/hamiltonianCycle.cpp
+++ src/Algorithms/hamiltonianCycle.cpp
@@ -5,7 +5,7 @@
 //Disjoint Union Set for cycle detection
 int findDSU(int a,int *rep)
 {
-    if(rep[a]==-1)return a;
+    if(rep[a]==a)return a;
     else return rep[a]=findDSU(rep[a],rep);
 }
 void unionDSU(int a,int b, int *rep)
@@ -17,10 +17,15 @@ void unionDSU(int a,int b, int *rep)
         rep[repA]=repB;
     }
 }
+int convertCoordsToIndex(Vector2 coords,int nodesPerRow)
+{
+    return coords.col + coords.row*nodesPerRow;
+}
+
 void kruskalMst(Grid *gridObj,sf::RenderWindow *window,CONFIG *config,std::vector<Vector2>*walls)
 {
     int nodesPerRow = gridObj->gridSize-2;
-    int rep[nodesPerRow*nodesPerRow];
+    int *rep = new int[nodesPerRow*nodesPerRow];
     for(int i=0;i<nodesPerRow*nodesPerRow;i++)
     {
         rep[i]= i;
@@ -29,10 +34,56 @@ void kruskalMst(Grid *gridObj,sf::RenderWindow *window,CONFIG *config,std::vecto
     std::random_device rd;
     std::mt19937 gen(rd());
     std::shuffle(walls->begin(), walls->end(), gen);
-    while(!walls.empty())
+
+    int dirX[] = {0,0,2,-2};
+    int dirY[] = {2,-2,0,0};
+    while(!walls->empty())
     {
-        //do kruskal logic
+        Vector2 parentTile = walls->back();
+        walls->pop_back();
+
+        int idx = convertCoordsToIndex(parentTile,nodesPerRow);
+        //std::cout<<idx<<std::endl;
+        int parent_rep = findDSU(idx,rep);
+        for(int i=0;i<4;i++)
+        {
+            int new_row = parentTile.row + dirY[i];
+            int new_col = parentTile.col + dirX[i];
+
+            bool isTileOnBoard = new_col>0&&new_col<nodesPerRow&&new_row>0&&new_row<nodesPerRow;
+            if(!isTileOnBoard)continue;
+
+            int nbr_idx = convertCoordsToIndex(Vector2(new_col,new_row),nodesPerRow);
+            int nbr_rep = findDSU(nbr_idx,rep);
+
+            if(nbr_rep!=parent_rep)
+            {
+
+                unionDSU(nbr_rep,parent_rep,rep);
+                if(dirX[i]==2)
+                {
+                    gridObj->grid[parentTile.row][parentTile.col+1].state = TileState::wall;
+                }
+                else if(dirX[i]==-2)
+                {
+                    gridObj->grid[parentTile.row][parentTile.col-1].state = TileState::wall;
+                }
+                else if(dirY[i]==2)
+                {
+                    gridObj->grid[parentTile.row+1][parentTile.col].state = TileState::wall;
+                }
+                else if(dirY[i]==-2)
+                {
+                    gridObj->grid[parentTile.row-1][parentTile.col].state = TileState::wall;
+                }
+                renderGrid(gridObj,window);
+                std::this_thread::sleep_for(std::chrono::milliseconds(101-config->visualizationSpeed));
+            }
+        }
+
+
     }
+    delete []rep;
 }
 
 void drawFrame(Grid *gridObj,sf::RenderWindow *window,CONFIG *config)
@@ -74,6 +125,6 @@ void visualizeHamiltonian(Grid *gridObj,sf::RenderWindow *window,CONFIG *config)
 
     drawFrame(gridObj,window,config);
     drawInitialTiles(gridObj,window,config,&walls);
-    kruskalMst(gridObj,window,config,&walls)
+    kruskalMst(gridObj,window,config,&walls);
 
 };
